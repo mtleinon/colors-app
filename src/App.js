010342d08a8c33c4e44ead151fd52634e46eadb8
mtleinon <mika.t.leinonen@gmail.com>
2019-05-20 20:12:14 +0300

    Added colorHelper module for generating colors.

--- src/App.js
+++ src/App.js
@@ -1,7 +1,10 @@
 import React from 'react';
 import Palette from './Palette';
 import seedColors from './seedColors';
+
+import generatePalette from './colorHelpers';
 function App() {
+  const palette = generatePalette(seedColors[0]);
   return (
     <div>
       <Palette palette={seedColors[4]} />
