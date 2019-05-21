996979073cd5820c44cc777ebb6f0bedfb39c175
mtleinon <mika.t.leinonen@gmail.com>
2019-05-21 18:57:57 +0300

    Styled color slider.

--- src/Palette.js
+++ src/Palette.js
@@ -1,10 +1,10 @@
 import React, { Component } from 'react';
-import './Palette.css';
 import ColorBox from './ColorBox';
 import Slider from 'rc-slider';
 import 'rc-slider/assets/index.css';
 import Select from '@material-ui/core/Select';
 import MenuItem from '@material-ui/core/MenuItem';
+import './Palette.css';
 
 export default class Palette extends Component {
   constructor(props) {
@@ -25,13 +25,15 @@ export default class Palette extends Component {
     ));
     return (
       <div className="Palette">
-        <Slider
-          defaultValue={level}
-          min={100}
-          max={900}
-          step={100}
-          onAfterChange={this.changeLevel}
-        />
+        <div className="slider">
+          <Slider
+            defaultValue={level}
+            min={100}
+            max={900}
+            step={100}
+            onAfterChange={this.changeLevel}
+          />
+        </div>
         {/* Navbar goes here */}
         <div className="Palette-colors">{colorBoxes}</div>
         {/* Footer here */}
