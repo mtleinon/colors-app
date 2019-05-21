ed8decc30b291f16ca4c829984731314cc2fe60f
mtleinon <mika.t.leinonen@gmail.com>
2019-05-21 20:19:30 +0300

    Add Navbar and font.

--- src/Palette.js
+++ src/Palette.js
@@ -1,9 +1,8 @@
 import React, { Component } from 'react';
 import ColorBox from './ColorBox';
-import Slider from 'rc-slider';
-import 'rc-slider/assets/index.css';
 import Select from '@material-ui/core/Select';
 import MenuItem from '@material-ui/core/MenuItem';
+import Navbar from './Navbar';
 import './Palette.css';
 
 export default class Palette extends Component {
@@ -21,20 +20,11 @@ export default class Palette extends Component {
     const { colors } = this.props.palette;
     const { level } = this.state;
     const colorBoxes = colors[level].map(color => (
-      <ColorBox color={color.hex} name={color.name} />
+      <ColorBox color={color.hex} key={color.name} name={color.name} />
     ));
     return (
       <div className="Palette">
-        <div className="slider">
-          <Slider
-            defaultValue={level}
-            min={100}
-            max={900}
-            step={100}
-            onAfterChange={this.changeLevel}
-          />
-        </div>
-        {/* Navbar goes here */}
+        <Navbar level={level} changeLevel={this.changeLevel} />
         <div className="Palette-colors">{colorBoxes}</div>
         {/* Footer here */}
       </div>
