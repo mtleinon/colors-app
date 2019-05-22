239452aefe06490129c4126c3ed680009a429be3
mtleinon <mika.t.leinonen@gmail.com>
2019-05-22 09:38:00 +0300

    Routing added.

--- src/PaletteList.js
+++ src/PaletteList.js
@@ -3,18 +3,6 @@ import { Link } from 'react-router-dom';
 // import './PaletteList.css';
 
 export default class PaletteList extends Component {
-  // constructor(props) {
-  // super(props);
-
-  // this.state = {
-  // : '',
-  // };
-  // }
-
-  // handleChange = e => {
-  // this.setState({ [e.target.name]: e.target.value });
-  // };
-
   render() {
     return (
       <div>
