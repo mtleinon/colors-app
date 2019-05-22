a7b8cf2712e5785ea87115ffff738508f38fbf96
mtleinon <mika.t.leinonen@gmail.com>
2019-05-22 09:39:28 +0300

    Add palette list and Links.

--- src/PaletteList.js
+++ src/PaletteList.js
@@ -1,6 +1,5 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
-// import './PaletteList.css';
 
 export default class PaletteList extends Component {
   render() {
