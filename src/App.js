cec9698c252de124db5f2c68a2b3c99f84399905
mtleinon <mika.t.leinonen@gmail.com>
2019-06-03 12:52:54 +0300

    Removed unused import.

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Route, Switch, Redirect } from 'react-router-dom';
+import { Route, Switch } from 'react-router-dom';
 import { TransitionGroup, CSSTransition } from 'react-transition-group';
 import Palette from './Palette';
 import seedColors from './seedColors';
